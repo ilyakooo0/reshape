b92aae4f9cbe74707cd0615a158211d78f4372bb
iko <mail@iko.soy>
2023-02-13 09:59:47 +0300

    Added yet more quotes

--- src/migrations/add_foreign_key.rs
+++ src/migrations/add_foreign_key.rs
@@ -48,7 +48,7 @@ impl Action for AddForeignKey {
             ALTER TABLE "{table}"
             ADD CONSTRAINT {constraint_name}
             FOREIGN KEY ({columns})
-            REFERENCES {referenced_table} ({referenced_columns})
+            REFERENCES "{referenced_table}" ({referenced_columns})
             ON DELETE {on_delete}
             ON UPDATE {on_update}
             NOT VALID
